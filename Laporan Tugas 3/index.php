39776a3de1d669ed3137d48422ddfdeff66f1a75
Firja Rakha Adwittya <firjarakhaa@gmail.com>
2024-09-20 11:38:10 +0700

    upload

---
+++ Laporan Tugas 3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Landing Page - Buku Tamu</title>
+    </head>
+    <body>
+        <h1>Selamat Datang di Buku Tamu</h1>
+         <h1>Selamat datang di situs buku tamu kami. Silahkan mengisi buku tamu atau melihat daftar tamu yang telah mengisi.</h1>
+
+    <ul>
+        <li><a href="input.php">Isi Buku Tamu</a></li>
+        <li><a href="tabel.php">Lihat Daftar Tamu</a></li>
+        </ul>
+        </body>
+</html>
