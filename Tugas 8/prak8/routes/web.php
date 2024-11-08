e6127a12fa579f32c1dd15d69ed3822caecba9e9
FirjaRakha <firjarakhaa@gmail.com>
2024-11-08 23:15:18 +0700

    Tugas 8

---
+++ Tugas 8/prak8/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('about', [AboutController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/gallery', [GalleryController::class, 'index']);
+
+
+
+
+
